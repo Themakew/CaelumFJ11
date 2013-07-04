d8057eb8e7d263159e8e2fe296fe3f6d2011863b
Ruyther <ruy_fusion@hotmail.com>
2013-07-04 19:05:09 -0300

    Create Programa.java

---
+++ Programa.java
@@ -0,0 +1,15 @@
+
+public class Programa implements Runnable{
+  private int id;
+	
+	public void run(){
+		for(int i = 0; i<1000;i++){
+			System.out.println("Programa " + id + " valor: " + i);
+		}
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
